c6c5f56906f737816946b6531df2d9cea8e2e3c6
doyeong-kim <k2767dy@naver.com>
2022-09-28 13:51:17 +0900

    pv3

--- demoRE.py
+++ demoRE.py
@@ -4,7 +4,7 @@ import re
 
 #print(dir(re))
 
-result = re.search("[0~9]*th", "  35th")
+result = re.search("[0~9]*th", "35th")
 #매칭 오브젝트
 print(result)
 print(result.group())
@@ -14,3 +14,10 @@ print(result.group())
 # print(result)
 # print(result.group())
 
+result = re.search("apple", "this is apple")
+print(result.group())
+
+print("--년도찾기--")
+print( bool(re.search("\d{4}", "올해는 2022년")))
+result = re.search("\d{5}", "우리 동네는 52300")
+print(result.group())
