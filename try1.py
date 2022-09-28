134e5a1131e9fc80b76e33d5951d46f9afad991d
doyeong-kim <k2767dy@naver.com>
2022-09-28 10:22:15 +0900

    문자열 서식지정

--- try1.py
+++ try1.py
@@ -2,8 +2,16 @@
 
 def divide(a,b):
     return a/b
-
-result = divide(5,0)
-print("결과 : {0}".format(result))
+#에러처리
+try:
+    result = divide(5,0)
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다")
+except TypeError:
+    print("숫자여야 연산이 됩니다.")
+else:
+    print("결과 : {0}".format(result))
+finally:
+    print("무조건 실행")
 
 print("-----전체 코드 종료 ----")
