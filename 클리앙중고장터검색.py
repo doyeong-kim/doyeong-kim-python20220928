6c27bc056d822a2d898c7a91568a7e98279d0b07
doyeong-kim <k2767dy@naver.com>
2022-09-29 13:18:37 +0900

    중고장터 키워드 검색

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -6,6 +6,8 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+f = open("c:\\work\\clien.txt", "wt", encoding="utf-8")
+
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
@@ -23,9 +25,11 @@ for n in range(0,10):
         for item in list:
                 try:
                         title = item.text.strip()
-                        print(title)
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
+                        #print(title)
+                        if (re.search('아이폰', title)):
+                                 print(title.strip())
+                                 f.write(title.strip() + "\n")
                 except:
                         pass
         
+f.close()
